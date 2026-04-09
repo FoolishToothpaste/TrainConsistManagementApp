05457464b2e61054b1c3572653c46e3fead4eb1d
FoolishToothpaste <sohamroy947@gmail.com>
2026-04-09 14:07:06 +0530

    UseCase1

---
+++ TrainConsistManagementApp/src/appdata/UseCase1TrainConsistMgmnt.java
@@ -0,0 +1,20 @@
+package appdata;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseCase1TrainConsistMgmnt 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("=== Train Consist Management App ===");
+		
+		List<String> trainConsist = new ArrayList<>();
+
+		System.out.println("Train initialized successfully...");
+		System.out.println("Initial Bogie Count : " + trainConsist.size());
+	    System.out.println("Current Train Consist : " + trainConsist);
+
+	    System.out.println("System ready for operations...");
+	 }
+}
+
